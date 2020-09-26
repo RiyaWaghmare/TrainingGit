0b73443d9d1b59a6a4afe249480815f80bf6e903
Riya Rajesh Waghmare <riya.waghmare@publicissapient.com>
2020-09-26 15:57:29 +0530

    maven commit

--- sum.java
+++
@@ -1,9 +0,0 @@
-public class AddTwoNumber{
-	public static void main(String[] args){
-		int num1=5, num2=20, sum;
-		sum=num1+num2;
-		System.out.println("Sum of these numbers:"+sum);
-	}
-}
-
-//Made changes using newBranch
